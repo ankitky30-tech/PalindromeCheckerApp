7dad21c1180b9af898e8b9eacad285f0ae18a7bb
ankitky30-tech <ankitky30@gmail.com>
2026-03-06 15:05:52 +0530

    ankitky30-tech

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,7 +1,15 @@
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
 
-        System.out.println(" Welcome to Palindrome Checker App ");
-        System.out.println("this is uc1");
+        // UC2: Hardcoded Palindrome Check
+        String word = "madam";   // hardcoded string
+        String reversed = new StringBuilder(word).reverse().toString();
+
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is NOT a palindrome.");
+        }
     }
 }
